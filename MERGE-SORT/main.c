3cab81f3dd184f8ce50e8d4f3c4299556fb9c33c
sidalei <suchao1991@gmail.com>
2015-05-25 10:44:43 +0800

    Initial Commit

---
+++ MERGE-SORT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MERGE-SORT
+//
+//  Created by Suchao on 15/5/25.
+//  Copyright (c) 2015年 Suchao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
